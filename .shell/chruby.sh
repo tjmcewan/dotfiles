d007313e24466e703916c638c20739ced7962b6d
Tim McEwan <tim@mcewan.it>
2014-05-23 15:53:36 +1000

    guard chruby stuff

--- .shell/chruby.sh
+++ .shell/chruby.sh
@@ -1,20 +1,24 @@
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+if [[ -d /usr/local/opt/chruby/share/chruby/ ]]; then
 
-# to set the default ruby to ruby 2:
-# echo "ruby-2" > ~/.ruby-version
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+  source /usr/local/opt/chruby/share/chruby/auto.sh
 
-if [[ -n "$ZSH_VERSION" ]]; then
-  if [[ "$preexec_functions" == *chruby_auto* ]] ; then
-    if [[ "$preexec_functions" != *add_binstubs_path* ]] ; then
-      preexec_functions+=("add_binstubs_path")
+  # to set the default ruby to ruby 2:
+  # echo "ruby-2" > ~/.ruby-version
+
+  if [[ -n "$ZSH_VERSION" ]]; then
+    if [[ "$preexec_functions" == *chruby_auto* ]] ; then
+      if [[ "$preexec_functions" != *add_binstubs_path* ]] ; then
+        preexec_functions+=("add_binstubs_path")
+      fi
     fi
   fi
-fi
 
-function add_binstubs_path() {
-  PATH=":$PATH:";
-  PATH="${PATH//:$BINSTUBS_PATH:/:}";
-  PATH="${PATH#:}"; PATH="${PATH%:}";
-  PATH="$BINSTUBS_PATH:$PATH";
-}
+  function add_binstubs_path() {
+    PATH=":$PATH:";
+    PATH="${PATH//:$BINSTUBS_PATH:/:}";
+    PATH="${PATH#:}"; PATH="${PATH%:}";
+    PATH="$BINSTUBS_PATH:$PATH";
+  }
+
+fi
